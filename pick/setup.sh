8099f4f875cd149a51c46ed366c770c5415a3949
Fabian Sandoval Saldias <fabianvss@gmail.com>
2025-05-19 21:04:04 +0200

    Add pick recipe

---
+++ pick/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$(dirname -- "$(dirname -- "${BASH_SOURCE[0]}")")"
+
+JUSTFILE=$(find "$PWD" -maxdepth 1 -type f -name "[Jj]ustfile" | head -n1)
+if [[ -z "$JUSTFILE" ]]; then
+  echo "Creating '$PWD/justfile'"
+  cat <<"EOF" >justfile
+import '$ROOT_DIR/core.just'
+import '$ROOT_DIR/pick/pick.just'
+
+_default:
+    just --list
+EOF
+else
+  echo "Updating '$JUSTFILE'"
+  # Create temporary file
+  TMPFILE=$(mktemp)
+  echo "import '$ROOT_DIR/pick/pick.just'" >"$TMPFILE"
+  cat "$JUSTFILE" >>"$TMPFILE"
+  mv "$TMPFILE" "$JUSTFILE"
+fi
+
+just pick
